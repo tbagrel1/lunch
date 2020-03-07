0b813d5aa13958833bca86f816eb886fafeea314
Thomas BAGREL <tomsb07@gmail.com>
2020-03-07 22:32:00 +0100

    Change `List` to `Iterable` for repositories

--- api/src/main/java/com/tbagrel1/lunch/api/db/repositories/AccountRepository.java
+++ api/src/main/java/com/tbagrel1/lunch/api/db/repositories/AccountRepository.java
@@ -4,10 +4,8 @@ import com.tbagrel1.lunch.api.db.models.Account;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface AccountRepository extends CrudRepository<com.tbagrel1.lunch.api.db.models.Account, Long> {
     Account findById(long id);
-    List<Account> findByFirstname(String firstname);
-    List<Account> findByLastname(String lastname);
+    Iterable<Account> findByFirstname(String firstname);
+    Iterable<Account> findByLastname(String lastname);
 }
